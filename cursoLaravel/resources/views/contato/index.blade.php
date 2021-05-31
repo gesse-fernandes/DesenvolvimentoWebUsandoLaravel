27c2a71d9af2000118e60232c0e56dc8ee7b2d7a
gesse-fernandes <gesse-fm@hotmail.com>
2021-05-31 19:04:17 -0300

    organizando os arquivos de templeade no front-end das Views

--- cursoLaravel/resources/views/contato/index.blade.php
+++ cursoLaravel/resources/views/contato/index.blade.php
@@ -1,7 +1,11 @@
-
-<h3>Você esta na view Index</h3>
+@extends('layout.site')
+@section('titulo','contatos')
+    
+@section('conteudo')
+<h3>Essa é a view do index</h3>
 @foreach($contatos as $contato)
 <p> {{$contato->nome}}</p>
-<p>{{$contato->telefone}}</p>
-   
+<p>{{$contato->telefone}}</p>   
 @endforeach
+@endsection
+
