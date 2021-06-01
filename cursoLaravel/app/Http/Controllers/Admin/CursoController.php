f5a755a8454c25adf2394a09b5833089e006d0af
gesse-fernandes <gesse-fm@hotmail.com>
2021-06-01 00:01:13 -0300

    criando um front end de materialize para trazer os dados e colocando as rotas de acordo

--- cursoLaravel/app/Http/Controllers/Admin/CursoController.php
+++ cursoLaravel/app/Http/Controllers/Admin/CursoController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Curso;
 class CursoController extends Controller
 {
-    //
+    public function index()
+    {
+        $registros = Curso::all();
+        return view('admin.cursos.index',compact('registros'));
+    }
 }
--- cursoLaravel/resources/views/layout/_includes/footer.blade.php
+++ cursoLaravel/resources/views/layout/_includes/footer.blade.php
@@ -1,2 +1,9 @@
-</body>
-</html>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script>
+$(document).ready(function(){
+   Materialize.updateTextFields();
+   $('.button-collapse').sideNav();
+});    
+</script>         
+    </body>
+  </html>
